45a3be3df067edb9cc1fde7827fe922b8480afb4
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-11-21 21:55:24 +0530

    Fixed Component: DsProgressTracker Step Types Fixed

--- src/Components/DsProgressTracker/DsProgressTracker.Types.ts
+++ src/Components/DsProgressTracker/DsProgressTracker.Types.ts
@@ -1,15 +1,14 @@
 import { DsBoxProps } from '../DsBox'
-import { DsProgressStepperProps } from './DsProgressStepper.Types'
-
-export interface DsProgressTrackerStepProps {
-  stepName: string
-}
+import {
+  DsProgressStepperProps,
+  DsProgressStepperStepProps
+} from './DsProgressStepper.Types'
 
 export interface DsProgressTrackerProps {
   /** This property can be used to switch between different variants of Progress Indicator where Default will provide a view with expand functionality on click, setsp only provide setp names without header and header provides a header with steps indicator without expandable stepper. */
   'ds-variant': 'default' | 'header' | 'steps'
 
-  steps: DsProgressTrackerStepProps[]
+  steps: DsProgressStepperStepProps[]
   activeStep: number
 
   StepperProps?: Omit<DsProgressStepperProps, 'activeStep' | 'steps' | 'ref'>
