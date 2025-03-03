f977a1a5de05fca58f94c66f847b774bdd1f6c83
Akshat Sharma <fc20294@Axis-FC-A00287.local>
2025-03-03 18:10:24 +0530

    figma connect components added

---
+++ src/Components/DsButton/DsButton.figma.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { DsButton } from "./DsButton.Component"
+import figma from "@figma/code-connect"
+import { DsRemixIcon } from "../DsRemixIcon"
+
+figma.connect(
+  DsButton,
+  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=3536%3A16589",
+  {
+    props: {
+      endIcon: figma.enum("➡️ Trailing icon", {
+        true: <DsRemixIcon className="ri-add-line" />
+      }),
+      startIcon: figma.enum("⬅️ Leading icon", {
+        true: <DsRemixIcon className="ri-add-line" />
+      }),
+      label: figma.textContent('button-text'),
+      color: figma.enum("⚙️ Variant", {
+        Primary: 'primary',
+        Secondary: 'secondary',
+        Flushed: 'primary'
+      }),
+      disabled: figma.enum("🛠️ State", {
+        Disabled: true
+      }),
+      size: figma.enum("📏 Size", {
+        L: "large",
+        M: "medium",
+        S: "small",
+      }),
+      onClick: () => {}
+    },
+    //@ts-ignore
+    example: (props) => <DsButton {...props} >{props.label}</DsButton>,
+  },
+)
+
+figma.connect(
+  DsButton,
+  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=3446-16543&m=dev",
+  {
+    props: {
+      leadingIcon: figma.boolean("⬅️ Leading icon"),
+      trailingIcon: figma.boolean("➡️ Trailing icon"),
+      label: figma.textContent('button-text'),
+      varianlt: 'text',
+      state: figma.enum("🛠️ State",{
+        Disable: 'disabled'
+      })
+    },
+    example: (props) => <DsButton {...props} >{props.label}</DsButton>,
+  },
+)
+
