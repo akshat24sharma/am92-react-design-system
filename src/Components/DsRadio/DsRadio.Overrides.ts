aaf74396b8666c3268f5b3191f562c856a462571
Akshat Sharma <fc20294@Axis-FC-A00287.local>
2025-03-11 19:38:18 +0530

    Fixed Component: DsRadio default state colour fixed

--- src/Components/DsRadio/DsRadio.Overrides.ts
+++ src/Components/DsRadio/DsRadio.Overrides.ts
@@ -2,7 +2,8 @@ export const DsRadioOverrides = {
   MuiRadio: {
     styleOverrides: {
       root: {
-        padding: 'var(--ds-spacing-quickFreeze)'
+        padding: 'var(--ds-spacing-quickFreeze)',
+        color: 'var(--ds-colour-iconDefault)'
       }
     }
   }
