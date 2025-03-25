ddf2abb34847c00eef9b739605f249402f51303e
Akshat Sharma <fc20294@Axis-FC-A00287.local>
2025-03-25 15:53:15 +0530

    Fixed Component: Helpertext prop default prop passing fixed

--- src/Components/DsHelperText/DsHelperText.Overrides.ts
+++ src/Components/DsHelperText/DsHelperText.Overrides.ts
@@ -1,7 +1,1 @@
-import { DsHelperTextDefaultProps } from "./DsHelperText.Types";
-
-export const DsHelperTextOverrides = {
-    MuiFormHelperText: {
-        defaultProps: DsHelperTextDefaultProps
-    }
-}
+export const DsHelperTextOverrides = {}
