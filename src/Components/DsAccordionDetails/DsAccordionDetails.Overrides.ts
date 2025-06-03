740ee7a837f8f73e28b9ba444f600dfa2d5e1ccc
Akshat Sharma <akshat.sharma@freecharge.com>
2025-06-03 20:36:52 +0530

    Fixed Styles: DsAccordion summary styles fixed

--- src/Components/DsAccordionDetails/DsAccordionDetails.Overrides.ts
+++ src/Components/DsAccordionDetails/DsAccordionDetails.Overrides.ts
@@ -3,7 +3,6 @@ export const DsAccordionDetailsOverrides = {
     styleOverrides: {
       root: {
         border: 'none',
-        borderBottom: '1px solid var(--ds-colour-strokeDefault)',
         padding: 'var(--ds-spacing-bitterCold)',
         fontWeight: 'var(--ds-typo-bodyRegularMedium-fontWeight)',
         fontSize: 'var(--ds-typo-bodyRegularMedium-fontSize)',
