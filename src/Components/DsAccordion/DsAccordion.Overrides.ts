baccab38983ab9d1698768f94a13d653c219bdb8
Akshat Sharma <akshat.sharma@freecharge.com>
2025-06-03 17:56:06 +0530

    Added Styles: DsAccordion list styles added

--- src/Components/DsAccordion/DsAccordion.Overrides.ts
+++ src/Components/DsAccordion/DsAccordion.Overrides.ts
@@ -8,21 +8,15 @@ export const DsAccordionOverrides = {
         backgroundColor: 'var(--ds-colour-surfacePrimary)',
         backgroundImage: 'none',
         overflow: 'hidden',
+        paddingTop: 'var(--ds-spacing-glacial)',
+        paddingBottom: 'var(--ds-spacing-glacial)',
         '&.Mui-expanded': {
-          margin: 'var(--ds-spacing-zero)'
-        },
-        '&.MuiAccordion-root ~ .MuiAccordion-root': {
-          paddingTop: 'var(--ds-spacing-glacial)',
-          paddingBottom: 'var(--ds-spacing-glacial)',
-        },
-        '&.MuiAccordion-root:has(+ .MuiAccordion-root)': {
-          paddingTop: 'var(--ds-spacing-glacial)',
-          paddingBottom: 'var(--ds-spacing-glacial)',
+            margin: 'var(--ds-spacing-zero)'
         },
         '&.MuiAccordion-root:not(:last-of-type)': {
-          borderBottom: '1px solid var(--ds-colour-stateDisabledSurface)',
+            borderBottom: '1px solid var(--ds-colour-stateDisabledSurface)',
         }
-      }
+    }
     }
   }
 }
