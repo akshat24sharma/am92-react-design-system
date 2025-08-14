7449b2ac11ad5fc31544111a85433c15b14dea05
Akshat Sharma <akshat.sharma@freecharge.com>
2025-08-14 11:15:56 +0530

    Fixed Type: DsAutocomplete default remixicon className fixed

--- src/Components/DsAutocomplete/DsAutocomplete.Types.tsx
+++ src/Components/DsAutocomplete/DsAutocomplete.Types.tsx
@@ -15,7 +15,7 @@ export const DsAutocompleteDefaultProps: DsAutocompleteProps<
   boolean
 > = {
   popupIcon: <DsRemixIcon className="ri-arrow-down-s-line" />,
-  clearIcon: <DsRemixIcon className="ri-close-line" />,
+  clearIcon: <DsRemixIcon className="ri-close-fill" />,
   multiple: false,
   disableClearable: false,
   freeSolo: false,
