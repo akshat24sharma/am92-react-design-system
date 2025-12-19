58f1563eb43687e34b9563a877e6a98c959390b3
FC476961 <Anjali8.Singh@freecharge.com>
2025-12-19 20:48:12 +0530

    date picker improvements

--- src/x-datepicker/Components/DsDateRangePicker/DateRangePickerActionBar.tsx
+++ src/x-datepicker/Components/DsDateRangePicker/DateRangePickerActionBar.tsx
@@ -1,52 +1,52 @@
 import {
   usePickerActionsContext,
   usePickerContext,
-  usePickerTranslations
-} from '@mui/x-date-pickers'
+  usePickerTranslations,
+} from "@mui/x-date-pickers";
 
-import type { IDateRangePickerActionBarProps } from './DsDateRangePicker.Types'
-import { DsButton, DsButtonGroup } from '../../../Components'
+import type { IDateRangePickerActionBarProps } from "./DsDateRangePicker.Types";
+import { DsButton, DsButtonGroup } from "../../../Components";
 
 export function DateRangePickerActionBar(
   props: IDateRangePickerActionBarProps
 ) {
-  const { acceptValueChanges } = usePickerActionsContext()
-  const { view } = usePickerContext()
-  const { startDate, endDate, onClear } = props
+  const { acceptValueChanges } = usePickerActionsContext();
+  const { view } = usePickerContext();
+  const { startDate, endDate, onClear, actions } = props;
 
-  const actions = view === 'day' ? ['clear', 'accept'] : []
+  const currentActions = view === "day" ? actions : [];
 
-  if (actions == null || actions?.length === 0) {
-    return null
+  if (currentActions == null || currentActions?.length === 0) {
+    return null;
   }
 
-  const translations = usePickerTranslations()
+  const translations = usePickerTranslations();
 
-  const isClearVisible = actions.includes('clear')
-  const isConfirmVisible = actions.includes('accept')
+  const isClearVisible = currentActions.includes("clear");
+  const isConfirmVisible = currentActions.includes("accept");
 
   return (
     <DsButtonGroup
       sx={{
-        gridArea: '3 / 1 / auto / 4',
-        backgroundColor: 'var(--ds-colour-surfaceSecondary)',
-        px: 'var(--ds-spacing-mild)',
-        borderTop: '1px solid var(--ds-colour-strokeDefault)',
+        gridArea: "3 / 1 / auto / 4",
+        backgroundColor: "var(--ds-colour-surfaceSecondary)",
+        px: "var(--ds-spacing-mild)",
+        borderTop: "1px solid var(--ds-colour-strokeDefault)",
         borderRadius:
-          'var(--ds-spacing-zero) var(--ds-spacing-zero) var(--ds-spacing-bitterCold) var(--ds-spacing-bitterCold) '
+          "var(--ds-spacing-zero) var(--ds-spacing-zero) var(--ds-spacing-bitterCold) var(--ds-spacing-bitterCold) ",
       }}
-      justifyContent='space-between'
-      size='medium'
+      justifyContent="space-between"
+      size="medium"
     >
       {isClearVisible && (
         <DsButton
           sx={{
-            py: 'var(--ds-spacing-glacial)',
-            px: 'var(--ds-spacing-pleasant)'
+            py: "var(--ds-spacing-glacial)",
+            px: "var(--ds-spacing-pleasant)",
           }}
-          variant='text'
-          size='medium'
-          color='secondary'
+          variant="text"
+          size="medium"
+          color="secondary"
           onClick={onClear}
           disabled={!startDate}
         >
@@ -56,12 +56,12 @@ export function DateRangePickerActionBar(
       {isConfirmVisible && (
         <DsButton
           sx={{
-            py: 'var(--ds-spacing-glacial)',
-            px: 'var(--ds-spacing-pleasant)'
+            py: "var(--ds-spacing-glacial)",
+            px: "var(--ds-spacing-pleasant)",
           }}
-          variant='text'
-          size='medium'
-          color='secondary'
+          variant="text"
+          size="medium"
+          color="secondary"
           onClick={acceptValueChanges}
           disabled={!startDate || !endDate}
         >
@@ -69,5 +69,5 @@ export function DateRangePickerActionBar(
         </DsButton>
       )}
     </DsButtonGroup>
-  )
+  );
 }
--- src/x-datepicker/Components/DsDateRangePicker/DateRangePickerHeader.tsx
+++ src/x-datepicker/Components/DsDateRangePicker/DateRangePickerHeader.tsx
@@ -1,148 +1,135 @@
-import React from 'react'
+import React, { useMemo } from "react";
 
-import { useThemeProps } from '@mui/system'
+import { useThemeProps } from "@mui/system";
 import {
   usePickerActionsContext,
-  usePickerTranslations
-} from '@mui/x-date-pickers'
-import { useUtils } from '@mui/x-date-pickers/internals'
+  usePickerTranslations,
+} from "@mui/x-date-pickers";
+import { useUtils } from "@mui/x-date-pickers/internals";
 
-import type { IDateRangePickerHeaderProps } from './DsDateRangePicker.Types'
-import { DsButtonBase, DsIconButton, DsRemixIcon, DsStack, DsTypography } from '../../../Components'
+import type { IDateRangePickerHeaderProps } from "./DsDateRangePicker.Types";
+import {
+  DsButtonBase,
+  DsIconButton,
+  DsRemixIcon,
+  DsStack,
+  DsTypography,
+} from "../../../Components";
 
 export const DateRangePickerHeader = React.forwardRef(
-  function DatePickerToolbar<TDate extends Date>(
-    inProps: IDateRangePickerHeaderProps<TDate>,
+  function DatePickerToolbar(
+    inProps: IDateRangePickerHeaderProps,
     ref: React.Ref<HTMLDivElement>
   ) {
     const props = useThemeProps({
       props: inProps,
-      name: 'MuiDatePickerToolbar'
-    })
+      name: "MuiDatePickerToolbar",
+    });
 
-    const { startDate, endDate, activeField, onFieldChange } = props
-    const { cancelValueChanges } = usePickerActionsContext()
+    const { startDate, endDate, activeField, onFieldChange } = props;
+    const { cancelValueChanges } = usePickerActionsContext();
 
-    const translations = usePickerTranslations()
-    const utils = useUtils()
+    const translations = usePickerTranslations();
+    const utils = useUtils();
+
+    const createDateFieldButton = (
+      field: "start" | "end",
+      label: string,
+      date: Date | null,
+      isDisabled?: boolean
+    ) => (
+      <DsButtonBase
+        key={field}
+        onClick={() => !isDisabled && onFieldChange(field)}
+        disabled={isDisabled}
+        sx={{
+          flex: 1,
+          justifyContent: "flex-start",
+          py: "var(--ds-spacing-bitterCold)",
+          pl: "var(--ds-spacing-bitterCold)",
+          borderBottom:
+            activeField === field
+              ? "1px solid var(--ds-colour-actionSecondary)"
+              : undefined,
+          backgroundColor: isDisabled
+            ? "var(--ds-colour-stateDisabledSurface)"
+            : activeField === field
+            ? "var(--ds-colour-surfacePrimary) !important"
+            : "var(--ds-colour-surfaceSecondary) !important",
+          borderRadius: 0,
+          "&.MuiButton-containedPrimary:disabled": {
+            backgroundColor: "var(--ds-colour-stateDisabledSurface)",
+          },
+        }}
+      >
+        <DsTypography
+          color="var(--ds-colour-typoPrimary)"
+          variant="bodyBoldSmall"
+          sx={{
+            marginRight: "var(--ds-spacing-glacial)",
+          }}
+        >
+          {label}
+        </DsTypography>
+        <DsTypography
+          variant="bodyBoldSmall"
+          color="var(--ds-colour-typoPrimary)"
+        >
+          {date
+            ? utils.formatByString(date, utils.formats.fullDate)
+            : "Pick a Date"}
+        </DsTypography>
+      </DsButtonBase>
+    );
+
+    const dateFieldButtons = useMemo(
+      () => [
+        createDateFieldButton("start", "Start", startDate),
+        createDateFieldButton("end", "End", endDate, !startDate),
+      ],
+      [startDate, endDate, activeField, onFieldChange, utils]
+    );
 
     return (
       <DsStack
         ref={ref}
         sx={{
-          gridArea: '1 / 2 / auto / 4',
-          borderBottom: '1px solid var(--ds-colour-strokeDefault)'
+          gridArea: "1 / 2 / auto / 4",
+          borderBottom: "1px solid var(--ds-colour-strokeDefault)",
         }}
       >
         <DsStack
           sx={{
             borderRadius:
-              'var(--ds-spacing-bitterCold) var(--ds-spacing-bitterCold) var(--ds-spacing-zero) var(--ds-spacing-zero)',
-            backgroundColor: 'var(--ds-colour-surfaceSecondary)',
-            padding: 'var(--ds-spacing-cool) var(--ds-spacing-bitterCold)'
+              "var(--ds-spacing-bitterCold) var(--ds-spacing-bitterCold) var(--ds-spacing-zero) var(--ds-spacing-zero)",
+            backgroundColor: "var(--ds-colour-surfaceSecondary)",
+            padding: "var(--ds-spacing-cool) var(--ds-spacing-bitterCold)",
           }}
         >
           <DsTypography
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between'
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
             }}
-            variant='headingBoldExtraSmall'
+            variant="headingBoldExtraSmall"
           >
             {translations.datePickerToolbarTitle}
             <DsIconButton onClick={cancelValueChanges}>
-              <DsRemixIcon className='ri-close-line' />
+              <DsRemixIcon className="ri-close-line" />
             </DsIconButton>
           </DsTypography>
         </DsStack>
         <DsStack
-          direction='row'
+          direction="row"
           sx={{
-            width: '100%',
-            borderTop: '1px solid var(--ds-colour-strokeDefault)'
+            width: "100%",
+            borderTop: "1px solid var(--ds-colour-strokeDefault)",
           }}
         >
-          <DsButtonBase
-            onClick={() => onFieldChange('start')}
-            sx={{
-              flex: 1,
-              justifyContent: 'flex-start',
-              py: 'var(--ds-spacing-bitterCold)',
-              pl: 'var(--ds-spacing-bitterCold)',
-              borderBottom:
-                activeField === 'start'
-                  ? '1px solid var(--ds-colour-actionSecondary)'
-                  : undefined,
-              backgroundColor:
-                activeField === 'start'
-                  ? 'var(--ds-colour-surfacePrimary) !important'
-                  : 'var(--ds-colour-surfaceSecondary) !important',
-              borderRadius: 0
-            }}
-          >
-            <DsTypography
-              color='var(--ds-colour-typoPrimary)'
-              variant='bodyBoldSmall'
-              sx={{
-                marginRight: 'var(--ds-spacing-glacial)'
-              }}
-            >
-              Start
-            </DsTypography>
-            <DsTypography
-              variant='bodyBoldSmall'
-              color='var(--ds-colour-typoPrimary)'
-            >
-              {startDate
-                ? utils.formatByString(startDate, utils.formats.fullDate)
-                : 'Pick a Date'}
-            </DsTypography>
-          </DsButtonBase>
-
-          <DsButtonBase
-            onClick={() => startDate && onFieldChange('end')}
-            sx={{
-              flex: 1,
-              justifyContent: 'flex-start',
-              py: 'var(--ds-spacing-bitterCold)',
-              pl: 'var(--ds-spacing-bitterCold)',
-              borderBottom:
-                activeField === 'end'
-                  ? '1px solid var(--ds-colour-actionSecondary)'
-                  : undefined,
-              backgroundColor: startDate
-                ? activeField === 'end'
-                  ? 'var(--ds-colour-surfacePrimary) !important'
-                  : 'var(--ds-colour-surfaceSecondary) !important'
-                : 'var(--ds-colour-stateDisabledSurface)',
-              borderRadius: 0,
-              '&.MuiButton-containedPrimary:disabled': {
-                backgroundColor: 'var(--ds-colour-stateDisabledSurface)'
-              }
-            }}
-            disabled={!startDate}
-          >
-            <DsTypography
-              variant='bodyBoldSmall'
-              color='var(--ds-colour-typoPrimary)'
-              sx={{
-                marginRight: 'var(--ds-spacing-glacial)'
-              }}
-            >
-              End
-            </DsTypography>
-            <DsTypography
-              variant='bodyBoldSmall'
-              color='var(--ds-colour-typoPrimary)'
-            >
-              {endDate
-                ? utils.formatByString(endDate, utils.formats.fullDate)
-                : 'Pick a Date'}
-            </DsTypography>
-          </DsButtonBase>
+          {dateFieldButtons}
         </DsStack>
       </DsStack>
-    )
+    );
   }
-)
+);
