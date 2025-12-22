7969578dc831787dda94571a34523ea70b8ecbde
khushi <khushiahuja567@gmail.com>
2025-12-22 12:46:25 +0530

    added component: DsFab, DsFileUploader, DsLink, DsMenuItem

---
+++ src/Components/DsLink/DsLink.figma.tsx
@@ -0,0 +1,30 @@
+import figma from "@figma/code-connect"
+import { DsLink } from "./DsLink.Component"
+
+figma.connect(
+    DsLink,
+    "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=5055-29652&m=dev",
+    {
+        props: {
+            underline: figma.enum("⚙️ Variant", {
+                Text: 'hover',
+                'Inline Text': 'always',
+            }),
+            disabled: figma.enum("🛠️ State", {
+                Disabled: true,
+                Hover: false,
+                Active: false,
+                Focus: false,
+                Visited: false,
+            }),
+            // FIXME: text layer doent have a name in figma, so we use children as a workaround
+            children: figma.children('*'),
+        },
+        //@ts-ignore
+        example: ({ children, ...props}) => {
+            return (
+                <DsLink {...props}>{children}</DsLink>
+            )
+        },
+    },
+)
