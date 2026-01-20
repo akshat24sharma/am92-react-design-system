3f3c873542f4670934b62e2578d2cff1287b70d6
YOUR_GITHUB_USERNAME <akshat24sharma@gmail.com>
2026-01-20 15:51:19 +0530

    Added Code Connect: DsBottomSheet added in figma code connect

--- src/Components/DsBottomSheet/DsBottomSheet.figma.tsx
+++ src/Components/DsBottomSheet/DsBottomSheet.figma.tsx
@@ -5,10 +5,19 @@ import { DsBottomSheet } from "./DsBottomSheet.Component"
 
 figma.connect(
   DsBottomSheet,
-  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=51314-1715&m=dev",
+  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=51283-31501&m=dev",
   {
     props: {
-      title: figma.textContent("main_heading")
+      title: figma.textContent("main_heading"),
+      kicker: figma.textContent("kicker"),
+      description: figma.textContent("Supporting description copy goes here."),
+      showClose: figma.boolean("Close Button", {
+        true: true,
+        false: false,
+      }),
+      'primaryButtonText': 'Primary Button',
+      'secondaryButtonText': 'Secondary Button',
+      'primaryButtonProps': { onClick: () => {} }
     },
     example: (props) => <DsBottomSheet {...props} />,
   },
