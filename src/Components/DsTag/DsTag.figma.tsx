57c3907bc780bada634e5f9424c93986a50598ac
YOUR_GITHUB_USERNAME <akshat24sharma@gmail.com>
2026-02-06 12:37:34 +0530

    Added Code Connect: DsTag component code connect mappings added

--- src/Components/DsTag/DsTag.figma.tsx
+++ src/Components/DsTag/DsTag.figma.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import figma from "@figma/code-connect";
 import { DsTag } from "./DsTag.Component";
+import { DsRemixIcon } from "../DsRemixIcon";
 
 figma.connect(
   DsTag,
-  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=4161-26660&m=dev",
+  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=3027-14879&m=dev",
   {
     props: {
       selected: figma.enum("State", {
         Selected: true,
+      }),
+      disabled: figma.enum("State", {
+        Disabled: true,
+      }),
+      icon: figma.boolean("Leading Icon", {
+        true: <DsRemixIcon className="ri-poker-hearts-fill" />,
+        false: undefined
+      }),
+      onDelete: figma.boolean("Trailing Icon", {
+        true: () => {},
+        false: undefined
       }),
       value: figma.textContent('text')
     },
