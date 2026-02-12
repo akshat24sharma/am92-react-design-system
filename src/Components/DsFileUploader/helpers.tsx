9a32543661acbd2826402a07d00715924bc08965
khushi <khushiahuja567@gmail.com>
2026-02-12 17:46:17 +0530

    updated hook name for nonce

--- src/Components/DsFileUploader/helpers.tsx
+++ src/Components/DsFileUploader/helpers.tsx
@@ -11,7 +11,6 @@ import { DsFileUploaderImagePreview } from './DsFileUploaderPreview/DsFileUpload
 import { getFileValidator } from './validator'
 import { fileToFileUploader } from './converter'
 import { DsRemixIcon } from '../DsRemixIcon'
-import { __unsafe_useEmotionCache } from '@emotion/react';
 
 export const mergeProps = <T extends Record<string, any>>(
   inProps: Partial<T>,
@@ -137,8 +136,3 @@ export const getValidProcessedFile = async <
 
   return { valid, invalid }
 }
-
-export const getEmotionNonce = (): string | undefined => {
-  const cache = __unsafe_useEmotionCache();
-  return cache?.nonce;
-};
---
+++ src/Hooks/nonce.ts
@@ -0,0 +1,7 @@
+
+import { __unsafe_useEmotionCache } from '@emotion/react';
+
+export const useEmotionNonce = (): string | undefined => {
+    const cache = __unsafe_useEmotionCache();
+    return cache?.nonce;
+};
