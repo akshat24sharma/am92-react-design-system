56bbbb806e5acc88b6090fae999c9560441853a6
FC476961 <Anjali8.Singh@freecharge.com>
2026-04-01 13:34:37 +0530

    Added Component: DsPickersDay exported mui pickersday component

---
+++ src/x-datepicker/Components/DsPickersDay/DsPickersDay.Types.ts
@@ -0,0 +1,5 @@
+import { PickersDayProps } from "@mui/x-date-pickers";
+
+export interface DsPickersDayProps<TDate extends Date> extends PickersDayProps<TDate> {}
+
+export const DsPickersDayDefaultProps = {}
