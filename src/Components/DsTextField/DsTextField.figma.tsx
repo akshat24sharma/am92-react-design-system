e83c34f99206536631d18541ac677664b92865fe
YOUR_GITHUB_USERNAME <akshat24sharma@gmail.com>
2026-04-27 14:31:09 +0530

    Added figma code connect DsTextfield start adornment added

--- src/Components/DsTextField/DsTextField.figma.tsx
+++ src/Components/DsTextField/DsTextField.figma.tsx
@@ -10,25 +10,29 @@ figma.connect(
   "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=1882-12581&m=dev",
   {
     props: {
-      error: figma.enum('🛠️ State', {
+      error: figma.enum('State', {
         Error: true,
       }),
-      success: figma.enum('🛠️ State', {
+      success: figma.enum('State', {
         Success: true
       }),
-      disabled: figma.enum('🛠️ State', {
+      disabled: figma.enum('State', {
         Disabled: true
       }),
-      readOnly: figma.enum('🛠️ State', {
+      readOnly: figma.enum('State', {
         Display: true
       }),
-      helperText: figma.enum('🛠️ State', {
+      helperText: figma.enum('State', {
         Error: figma.textContent('status_text'),
         Success: figma.textContent('status_text')
       }),
       label: figma.textContent('label_text'),
-      endAdornment: figma.enum('➡️ Trailing icon', {
+      endAdornment: figma.enum('Trailing icon', {
         true: <DsRemixIcon className="ri-close-fill" />
+      }),
+      startAdornment: figma.boolean('Prefix', {
+        true: figma.textContent('prefix'),
+        false: undefined
       })
     },
     example: (props) => <DsTextField {...props} />,
