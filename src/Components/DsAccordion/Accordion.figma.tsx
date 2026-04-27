4261602817f5e3cd8e73fa3b4082484627aed6ff
YOUR_GITHUB_USERNAME <akshat24sharma@gmail.com>
2026-04-27 14:36:29 +0530

    merge conflicts resolved

--- src/Components/DsAccordion/Accordion.figma.tsx
+++ src/Components/DsAccordion/Accordion.figma.tsx
@@ -1,8 +1,12 @@
 import React from "react"
 import { DsAccordion } from "./DsAccordion.Component"
+<<<<<<< HEAD
 import { DsDivider } from "../DsDivider"
 import figma from "@figma/code-connect"
 import { DsStack } from "../DsStack"
+=======
+import figma from "@figma/code-connect"
+>>>>>>> 46f1f92 (v2.x update)
 
 figma.connect(
   DsAccordion,
@@ -11,29 +15,9 @@ figma.connect(
     props: {
       header: figma.textContent('heading_text'),
       summary: figma.textContent('body-text'),
-      // children: figma.children(['header', 'body'])
+      children: figma.children('*')
     },
     //@ts-ignore
-    example: (props) => <DsAccordion header={props.header} summary={props.summary}/>,
-  },
-)
-
-// Accordion List Component
-figma.connect(
-  DsAccordion,
-  "https://www.figma.com/design/9o1qfErgy23YgsDzoaXpw7/Subzero-V.2.0-Design-System?node-id=14214-1502&m=dev",
-  {
-    props: {
-      header: figma.textContent('heading_text'),
-      summary: figma.textContent('body-text'),
-    },
-    example: (props) => (
-      <DsStack>
-        <DsAccordion {...props} />
-        <DsAccordion {...props} />
-        <DsAccordion {...props} />
-        <DsAccordion {...props} />
-      </DsStack>
-    ),
+    example: (props) => <DsAccordion header={props.header} summary={props.summary}>{props.children}</DsAccordion>,
   },
 )
--- src/Components/DsBox/DsBox.figma.tsx
+++ src/Components/DsBox/DsBox.figma.tsx
@@ -12,11 +12,10 @@ import figma from "@figma/code-connect"
 
 figma.connect(
   DsBox,
-  "https://www.figma.com/design/C9TREbo9kCnImBjcO1zRkG/Subzero-V.2.0-playground?node-id=12666-98079&m=dev",
+  "https://www.figma.com/design/C9TREbo9kCnImBjcO1zRkG/Subzero-V.2.0-playground?node-id=12666-98079",
   {
     props: {
-      children: figma.children('*'),
-      width: figma.string('properties')
+      children: figma.children('*')
     },
     example: (props) => <DsBox>{props.children}</DsBox>,
   },
