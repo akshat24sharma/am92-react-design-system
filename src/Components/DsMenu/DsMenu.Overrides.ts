ddb4228554d37154505c9971e1a139647c02fb79
khushi ahuja <khushiahuja567@gmail.com>
2026-07-16 11:47:58 +0530

    Fixed Component: Fixed background colour for DsMenu

--- src/Components/DsMenu/DsMenu.Overrides.ts
+++ src/Components/DsMenu/DsMenu.Overrides.ts
@@ -4,7 +4,10 @@ export const DsMenuOverrides = {
   MuiMenu: {
     defaultProps: DsMenuDefaultProps,
     styleOverrides: {
-      paper: { borderRadius: 'var(--ds-radius-glacial)' },
+      paper: { 
+        borderRadius: 'var(--ds-radius-glacial)',
+        background: 'var(--ds-colour-surfacePrimary)',
+      },
       list: { boxShadow: 'var(--ds-elevation-4)' }
     }
   }
